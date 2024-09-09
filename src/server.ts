b842b41b79597b020330aa7b5e0d0dd3a3c301e6
Victor Moreira <zvihugo@hotmail.com>
2024-09-09 18:56:29 -0300

    to github

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,68 @@
-console.log("hello world");
+import fastify from "fastify";
+
+const server = fastify({ logger: true });
+
+const teams = [
+  {
+    id: 1,
+    name: "McLaren",
+    base: "Woking, Surrey, England",
+  },
+  {
+    id: 2,
+    name: "Ferrari",
+    base: "Maranello, Emilia-Romagna, Italy",
+  },
+  {
+    id: 3,
+    name: "Mercedes",
+    base: "Brackley, Northamptonshire, England",
+  },
+];
+
+const drivers = [
+  { id: 1, name: "Michael Schumacher", team: "Ferrari" },
+  { id: 2, name: "Rubens Barrichello", team: "Ferrari" },
+  {
+    id: 3,
+    name: "Kimi Räikkönen",
+    team: "McLaren",
+  },
+  {
+    id: 4,
+    name: "Juan Pablo Montoya",
+    team: "McLaren",
+  },
+];
+
+server.get("/teams", async (req, res) => {
+  res.type("application/json").code(200);
+  return { teams };
+});
+
+server.get("/drivers", async (req, res) => {
+  res.type("application/json").code(200);
+  return {drivers};
+});
+
+interface routeParams {
+    id: string;
+}
+
+server.get<{Params: routeParams}>("/drivers/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const driver = drivers.find((driver => driver.id === id));
+
+    if(!driver){
+        res.type("application/json").code(404);
+        return {message: "Driver not found"};
+    }else{
+        res.type("application/json").code(200);
+        return {driver};
+    }
+});
+
+
+server.listen({ port: Number(process.env.PORT) || 3333 }, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
